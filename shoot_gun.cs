031de7b9b3fb2582e5caf8e186c16629a42be075
baraniski15 <b.baran@poczta.onet.pl>
2017-12-18 22:55:53 +0100

    'commit'

---
+++ shoot_gun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot_gun : MonoBehaviour {
+
+	public Rigidbody bullet;
+	public Transform bullet_transform;
+	public float pre_pocisku;
+
+	public void shoot () {
+		Rigidbody shoot_instant;
+		shoot_instant = Instantiate (bullet, bullet_transform.position, bullet_transform.rotation);
+		shoot_instant.AddForce (bullet_transform.forward * pre_pocisku);
+	}
+}
---
+++ shoot_mouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot_mouse : MonoBehaviour {
+
+	shoot_gun b;
+
+	// Use this for initialization
+	void Start () {
+		b = GetComponentInChildren<shoot_gun> ();
+		Debug.Log ("działam");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown (0)){
+			b.shoot ();
+		}
+	}
+}
